0d68e2365ffa67728778317a6eebfe614f8f3322
Milva <garangggg@gmail.com>
2024-08-18 00:21:45 +0900

    Add IAgent

---
+++ core-extensions/SabberStoneBasicAI/src/Interface/IAgent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SabberStoneCore.Conditions;
+using SabberStoneCore.Model;
+using SabberStoneCore.Tasks.PlayerTasks;
+using SabberStoneCore.Tasks.PlayerTasks.Lite;
+
+namespace SabberStoneBasicAI.Interface
+{
+	public interface IAgent
+	{
+		/// <summary>
+		/// The name of this agent.
+		/// </summary>
+		string Name { get; }
+		
+		/// <summary>
+		/// Calculate and return the best move with regard to the given game state.
+		/// </summary>
+		/// <param name="game">The game context.</param>
+		/// <returns>The action.</returns>
+		PlayerTaskLite GetAction(Game game);
+
+		/// <summary>
+		/// This method will be called when a match is started.
+		/// </summary>
+		void OnMatchStarted();
+
+		/// <summary>
+		/// This method will be called when each game (round) is started.
+		/// </summary>
+		void OnGameStarted();
+
+		/// <summary>
+		/// This method will be called when each game (round) is finished.
+		/// </summary>
+		void OnGameFinished();
+	}
+}
