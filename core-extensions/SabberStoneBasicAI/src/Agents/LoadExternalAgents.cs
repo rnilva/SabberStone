9508a2a65440e5ebebc723232606fddab8ae013d
Milva <rnilva849@gmail.com>
2024-08-22 14:20:30 +0900

    Add loading external agents from assemblies

---
+++ core-extensions/SabberStoneBasicAI/src/Agents/LoadExternalAgents.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SabberStoneBasicAI.Agents
+{
+    public static class FindAgents
+    {
+        private static Type[] GetAgentTypes()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            Assembly[] assemblies = currentDomain.GetAssemblies();
+
+            Type aiInterface = typeof(IAgent);
+
+            Type[] agents = assemblies
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => !type.IsInterface && 
+                               !type.IsAbstract &&
+                               type.GetInterfaces().Contains(aiInterface))
+                .ToArray();
+
+            foreach (Type agent in agents)
+                Console.WriteLine(agent.FullName);
+
+            return agents;
+        }
+
+        public static string[] Find()
+        {
+            Type[] types = GetAgentTypes();
+
+            return types.Select(t => t.AssemblyQualifiedName).ToArray();
+        }
+    }
+}
---
+++ pysabberstone-pythonnet/pysabberstone/load_external_agents.py
@@ -0,0 +1,49 @@
+import importlib
+from pathlib import Path
+
+from pysabberstone.interface.agent import DotNetAgentWrapper, Agent
+
+import pysabberstone.core
+from pysabberstone.core import clr
+from SabberStoneBasicAI.Agents import FindAgents
+
+
+class ExternalAgentMeta(type(DotNetAgentWrapper)):
+    def __new__(cls, name: str, bases: tuple, attrs: dict):
+        loaded_type = attrs["__loaded_agent__"]
+
+        def __init__(self, *args, **kwargs):
+            loaded_agent_instance = loaded_type(*args, **kwargs)
+            DotNetAgentWrapper.__init__(self, loaded_agent_instance)
+
+        attrs["__init__"] = __init__
+
+        return super().__new__(cls, name, (DotNetAgentWrapper,) + bases, attrs)
+
+
+def load():
+    dir = Path(__file__).parent / "external_agents"
+
+    for file in dir.iterdir():
+        if file.suffix == ".dll":
+            try:
+                clr.AddReference(str(file.resolve()))
+            except Exception as e:
+                print(e)
+            finally:
+                print(f"External module {file.name} is loaded.")
+
+    qualified_names: list[str] = list(FindAgents.Find())
+
+    loaded_agents: dict[str, type[DotNetAgentWrapper]] = {}
+    for q_name in qualified_names:
+        type_spec = q_name.split(",")[0]
+        *modules, class_name = type_spec.split(".")
+        module = importlib.import_module(".".join(modules))
+        cls = getattr(module, class_name)
+
+        agent_type = ExternalAgentMeta(type_spec, (), {"__loaded_agent__": cls})
+        loaded_agents[type_spec] = agent_type
+
+    return loaded_agents
+
