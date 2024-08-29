0748132bd3e53cd3e65176dd3b418070371470a8
Milva <rnilva849@gmail.com>
2024-08-29 13:01:57 +0900

    ✨(py) Support deckstring parsing

--- pysabberstone-pythonnet/pysabberstone/utils.py
+++ pysabberstone-pythonnet/pysabberstone/utils.py
@@ -1,23 +1,34 @@
-﻿import pysabberstone.core
-from pysabberstone.model.enums import FormatType
+﻿from pysabberstone.model.enums import FormatType
 from pysabberstone.py_types import Deck, CardClass
 
+import pysabberstone.core
+from SabberStoneCore.Config import DeckSerializer as _DeckSerializer
 from SabberStoneCore.Enums import CardClass as _CardClass, FormatType as _FormatType
 from SabberStoneCore.Model import Card as _Card, Cards as _Cards
-from System.Collections.Generic import List
+from System import ArgumentException as _ArgumentException
+from System.Collections.Generic import List as _List
 
 
-def convert_deck(deck: Deck, format: FormatType) -> tuple[_CardClass, List[_Card]]:
+def convert_deck(deck: Deck, format: FormatType) -> tuple[_CardClass, _List[_Card]]:
     card_class: _CardClass
-    cards: List[_Card]
+    cards: _List[_Card]
     if isinstance(deck, tuple):
         py_card_class, card_names = deck
         card_class = _CardClass(py_card_class)
-        cards = List[_Card](len(card_names))
+        cards = _List[_Card](len(card_names))
         for name in card_names:
             cards.Add(_Cards.FromName(name, _FormatType(int(format))))
     else:
-        raise NotImplementedError()
+        try:
+            _deck = _DeckSerializer.Deserialize(deck)
+        except _ArgumentException:
+            raise ValueError("Invalid deckstring.")
+
+        card_class = _deck.GetHero().Class
+        cards = _List[_Card](30)
+        for card_count_pair in _deck.GetCards():
+            for _ in range(card_count_pair.Value):
+                cards.Add(card_count_pair.Key)
 
     return card_class, cards
 
@@ -58,6 +69,8 @@ if __name__ == "__main__":
             "Frost Elemental",
         ],
     )
+
+    deck = "AAEDAaIHBvuWBPqgBIahBLWhBNyhBKWjBAz8lQT9lQTclgTqlgT4oATUoQTdoQTfoQTkoQTnoQTooQSTogQA"
 
     cls, core_deck = convert_deck(deck, FormatType.FT_CLASSIC)
     print(cls)
