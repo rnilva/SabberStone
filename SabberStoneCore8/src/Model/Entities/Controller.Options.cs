f4bd0e12693758d7d1bb3bf2602a628eba067013
Milva <rnilva849@gmail.com>
2024-09-11 16:21:08 +0900

    🚑 Fix Lite options target enumerator

--- SabberStoneCore8/src/Model/Entities/Controller.Options.cs
+++ SabberStoneCore8/src/Model/Entities/Controller.Options.cs
@@ -1426,31 +1426,44 @@ namespace SabberStoneCore.Model.Entities
 					[MethodImpl(MethodImplOptions.AggressiveInlining)]
 					get
 					{
+						// switch (_outerIndex)
+						// {
+						// 	case 0:
+						// 		if (!_targets.FriendlyHero)
+						// 		{
+						// 			++_outerIndex;
+						// 			goto case 1;
+						// 		}
+						// 		return 0;
+						// 	case 1:
+						// 		++_outerIndex;
+						// 		if (!_targets.EnemyHero)
+						// 		{
+						// 			goto case 2;
+						// 		}
+						// 		return 8;
+						// 	case 2:
+						// 		if (_targets.EnemyMinions.IsEmpty)
+						// 		{
+						// 			_outerIndex = 3;
+						// 			goto default;
+						// 		}
+						// 		return _targets.EnemyMinions[_innerIndex++] + 9;
+						// 	default:
+						// 		return _targets.FriendlyMinions[_innerIndex++] + 1;
+						// }
 						switch (_outerIndex)
 						{
 							case 0:
-								if (!_targets.FriendlyHero)
-								{
-									++_outerIndex;
-									goto case 1;
-								}
 								return 0;
 							case 1:
-								++_outerIndex;
-								if (!_targets.EnemyHero)
-								{
-									goto case 2;
-								}
 								return 8;
 							case 2:
-								if (_targets.EnemyMinions.IsEmpty)
-								{
-									_outerIndex = 3;
-									goto default;
-								}
 								return _targets.EnemyMinions[_innerIndex++] + 9;
-							default:
+							case 3:
 								return _targets.FriendlyMinions[_innerIndex++] + 1;
+							default:
+								throw new ArgumentOutOfRangeException("Enumerator Error");
 						}
 					}
 				}
@@ -1458,41 +1471,71 @@ namespace SabberStoneCore.Model.Entities
 				[MethodImpl(MethodImplOptions.AggressiveInlining)]
 				public bool MoveNext()
 				{
-					if (_outerIndex < 1)
-					{
-						++_outerIndex;
-						return true;
-					}
+					// if (_outerIndex < 1)
+					// {
+					// 	++_outerIndex;
+					// 	return true;
+					// }
 
-					if (_outerIndex == 1)
-					{
-						if (_targets.EnemyMinions.IsEmpty)
-						{
-							if (_targets.FriendlyMinions.IsEmpty)
-								return false;
-							_outerIndex = 3;
-							return true;
-						}
+					// if (_outerIndex == 1)
+					// {
+					// 	if (_targets.EnemyMinions.IsEmpty)
+					// 	{
+					// 		if (_targets.FriendlyMinions.IsEmpty)
+					// 			return false;
+					// 		_outerIndex = 3;
+					// 		return true;
+					// 	}
 
-						_outerIndex = 2;
-						return true;
-					}
+					// 	_outerIndex = 2;
+					// 	return true;
+					// }
 
-					if (_outerIndex == 2)
+					// if (_outerIndex == 2)
+					// {
+					// 	if (_innerIndex == _targets.EnemyMinions.Length)
+					// 	{
+					// 		if (_targets.FriendlyMinions.IsEmpty)
+					// 			return false;
+					// 		_innerIndex = 0;
+					// 		_outerIndex = 3;
+					// 		return true;
+					// 	}
+
+					// 	return true;
+					// }
+
+					// return _innerIndex != _targets.FriendlyMinions.Length;
+
+					switch (_outerIndex)
 					{
-						if (_innerIndex == _targets.EnemyMinions.Length)
-						{
-							if (_targets.FriendlyMinions.IsEmpty)
-								return false;
+						case -1:
+							++_outerIndex;
+							if (_targets.FriendlyHero)
+								return true;
+							goto case 0;
+						case 0:
+							++_outerIndex;
+							if (_targets.EnemyHero)
+								return true;
+							goto case 1;
+						case 1:
+							++_outerIndex;
+							if (_innerIndex != _targets.EnemyMinions.Length)
+								return true;
+							goto case 2;
+						case 2:
+							if (_innerIndex != _targets.EnemyMinions.Length)
+								return true;
+							++_outerIndex;
 							_innerIndex = 0;
-							_outerIndex = 3;
-							return true;
-						}
+							goto case 3;
+						case 3:
+							return _innerIndex != _targets.FriendlyMinions.Length;
+						default:
+							throw new ArgumentOutOfRangeException("Enumerator Error");
 
-						return true;
 					}
-
-					return _innerIndex != _targets.FriendlyMinions.Length;
 				}
 			}
 
